8af18209890055d7c2656c923553e7553fc94bdc
stanislav <silin.stanislav@outlook.com>
2023-12-17 17:17:40 +0200

    Create initial project.

---
+++ ZeroQLGitHubActions/Program.cs
@@ -0,0 +1,8 @@
+﻿
+var httpClient = new HttpClient();
+httpClient.BaseAddress = new Uri("http://localhost:10000/graphql");
+
+var client = new ZeroQL.Client.ZeroQLClient(httpClient);
+var response = await client.Query(q => q.Me(m => m.Id));
+
+Console.WriteLine(response.Data);
